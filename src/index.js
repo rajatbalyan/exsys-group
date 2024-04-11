a4f6d87fe959819b58b37e34d76942b0e7e6a9bd
Rajat Balyan <balyanrajat1812@gmail.com>
2024-04-11 17:05:05 +0530

    Update index.js with React Router v6 and enable performance measurements
    * Import and use `createHashRouter` and `RouterProvider` from 'react-router-dom'
    * Replace `BrowserRouter` with `HashRouter`
    * Import and use `reportWebVitals` from the app's entry point
    * Remove unused import of `App` and `styles/index.css`
    * Update the extension of the CSS file import to `.css`
    * Wrap the app in `React.StrictMode` for better development experience.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createHashRouter,
   RouterProvider
 } from 'react-router-dom';
-import App from './App';
-import './styles/index.css';
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
 const router = createHashRouter([
   {
@@ -19,3 +21,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
